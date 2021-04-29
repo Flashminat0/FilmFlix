e92ed9e18173af08f2999c6a4568626bca0dc629
Yasas Lowe <64888128+Flashminat0@users.noreply.github.com>
2021-04-29 06:29:02 +0530

    movie upload page finished

--- includes/form_3.php
+++ includes/form_3.php
@@ -1,5 +1,5 @@
 <?php	
-	if (empty($_POST['name']) && strlen($_POST['name']) == 0 || empty($_POST['email']) && strlen($_POST['email']) == 0 || empty($_POST['message']) && strlen($_POST['message']) == 0)
+	if (empty($_POST['name']) && strlen($_POST['name']) == 0 || empty($_POST['email']) && strlen($_POST['email']) == 0 || empty($_POST['message']) && strlen($_POST['message']) == 0 || empty($_POST['textarea_406']) && strlen($_POST['textarea_406']) == 0)
 	{
 		return false;
 	}
@@ -7,6 +7,7 @@
 	$name = $_POST['name'];
 	$email = $_POST['email'];
 	$message = $_POST['message'];
+	$textarea_406 = $_POST['textarea_406'];
 	$optin = $_POST['optin'];
 	
 	$to = 'receiver@yoursite.com'; // Email submissions are sent to this email
@@ -14,7 +15,7 @@
 	// Create email	
 	$email_subject = "Message from a Blocs website.";
 	$email_body = "You have received a new message. \n\n".
-				  "Name: $name \nEmail: $email \nMessage: $message \nOptin: $optin \n";
+				  "Name: $name \nEmail: $email \nMessage: $message \nTextarea_406: $textarea_406 \nOptin: $optin \n";
 	$headers = "MIME-Version: 1.0\r\nContent-type: text/plain; charset=UTF-8\r\n";	
 	$headers .= "From: contact@yoursite.com\n";
 	$headers .= "Reply-To: $email";	
